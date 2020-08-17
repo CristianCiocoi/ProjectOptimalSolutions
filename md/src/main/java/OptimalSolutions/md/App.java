885de26766860edd2937ffaf2878640d3f63d341
Cristian Ciocoi <cristian.c49575757@yahoo.com>
2020-08-18 02:31:40 +0300

    Reading CSV File
    need to fix the problem with split because the image string is splitted by comma too so it is proceded as two fields

--- md/src/main/java/OptimalSolutions/md/App.java
+++ md/src/main/java/OptimalSolutions/md/App.java
@@ -1,14 +1,83 @@
 package OptimalSolutions.md;
-import java.io.*;
-import java.nio.*;
-import java.nio.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+        List<InterviewFileFeeder> fileds = readFromCSV("Inter.csv");
+        // let's print all the person read from CSV file
+        
+        for (InterviewFileFeeder field : fileds) {
+            System.out.println(field);
+        }
     }
+    
+    private static List<InterviewFileFeeder> readFromCSV(String fileName) {
+        List<InterviewFileFeeder> fields = new ArrayList<InterviewFileFeeder>();
+        Path pathToFile = Paths.get(fileName);
+
+        // create an instance of BufferedReader
+        // using try with resource, Java 7 feature to close resources
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8)) {
+
+            // read the first line from the text file
+            String line = br.readLine();
+            
+
+            // loop until all lines are read
+            while (line != null) {
+
+                // use string.split to load a string array with the values from
+                // each line of
+                // the file, using a comma as the delimiter
+                String[] attributes = line.split(",");
+
+                InterviewFileFeeder field = createField(attributes);
+
+                // adding book into ArrayList
+                
+                fields.add(field);
+                // read next line before looping
+                // if end of file reached, line would be null
+                line = br.readLine();
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return fields;
+    }
+
+    private static InterviewFileFeeder createField(String[] metadata) {
+    	String nameA = metadata[0];
+        String surnameB = metadata[1];
+        String mailC = metadata[2];
+        String genderD = metadata[3];
+        String profileImageE = metadata[4];
+        String paymentMethodF = metadata[5];
+        String transactionsG = metadata[6];
+        boolean h = Boolean.parseBoolean(metadata[7]);
+        boolean i = Boolean.parseBoolean(metadata[8]);
+        String cityJ = metadata[9];
+        System.out.println(metadata.length);
+//my out of index error profileImageE="data:image/png;base64, paymentMethodF=iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9
+
+        // create and return book of this metadata
+        return new InterviewFileFeeder(nameA, surnameB, mailC, genderD, profileImageE, paymentMethodF, transactionsG, h, i, cityJ);
+         
+    }
+
 }
+    
+
---
+++ md/src/main/java/OptimalSolutions/md/InterviewFileFeeder.java
@@ -0,0 +1,120 @@
+package OptimalSolutions.md;
+
+public class InterviewFileFeeder {
+	private String nameA;
+	private String surnameB;
+	private String mailC;
+	private String genderD;
+	private String profileImageE;
+	private String paymentMethodF;
+	private String transactionsG;
+	private boolean h;
+	private boolean i;
+	private String cityJ;
+	
+	public InterviewFileFeeder(String nameA, String surnameB, String mailC, String genderD, String profileImageE,
+			String paymentMethodF, String transactionsG, boolean h, boolean i, String cityJ) {
+		super();
+		this.nameA = nameA;
+		this.surnameB = surnameB;
+		this.mailC = mailC;
+		this.genderD = genderD;
+		this.profileImageE = profileImageE;
+		this.paymentMethodF = paymentMethodF;
+		this.transactionsG = transactionsG;
+		this.h = h;
+		this.i = i;
+		this.cityJ = cityJ;
+	}
+
+	public String getNameA() {
+		return nameA;
+	}
+
+	public void setNameA(String nameA) {
+		this.nameA = nameA;
+	}
+
+	public String getSurnameB() {
+		return surnameB;
+	}
+
+	public void setSurnameB(String surnameB) {
+		this.surnameB = surnameB;
+	}
+
+	public String getMailC() {
+		return mailC;
+	}
+
+	public void setMailC(String mailC) {
+		this.mailC = mailC;
+	}
+
+	public String getGenderD() {
+		return genderD;
+	}
+
+	public void setGenderD(String genderD) {
+		this.genderD = genderD;
+	}
+
+	public String getProfileImageE() {
+		return profileImageE;
+	}
+
+	public void setProfileImageE(String profileImageE) {
+		this.profileImageE = profileImageE;
+	}
+
+	public String getPaymentMethodF() {
+		return paymentMethodF;
+	}
+
+	public void setPaymentMethodF(String paymentMethodF) {
+		this.paymentMethodF = paymentMethodF;
+	}
+
+	public String getTransactionsG() {
+		return transactionsG;
+	}
+
+	public void setTransactionsG(String transactionsG) {
+		this.transactionsG = transactionsG;
+	}
+
+	public boolean isH() {
+		return h;
+	}
+
+	public void setH(boolean h) {
+		this.h = h;
+	}
+
+	public boolean isI() {
+		return i;
+	}
+
+	public void setI(boolean i) {
+		this.i = i;
+	}
+
+	public String getCityJ() {
+		return cityJ;
+	}
+
+	public void setCityJ(String cityJ) {
+		this.cityJ = cityJ;
+	}
+
+	@Override
+	public String toString() {
+		return "InterviewFileFeeder [nameA=" + nameA + ", surnameB=" + surnameB + ", mailC=" + mailC + ", genderD="
+				+ genderD + ", profileImageE=" + profileImageE + ", paymentMethodF=" + paymentMethodF
+				+ ", transactionsG=" + transactionsG + ", Belovo=" + h + ", i=" + i + ", cityJ=" + cityJ + "]";
+	}
+
+
+	
+
+}
