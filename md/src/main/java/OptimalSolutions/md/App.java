70cc5bc7b399d0dc98111adc739cd10b301fcecc
Cristian Ciocoi <cristian.c49575757@yahoo.com>
2020-08-18 20:02:15 +0300

    Update

--- md/src/main/java/OptimalSolutions/md/App.java
+++ md/src/main/java/OptimalSolutions/md/App.java
@@ -13,14 +13,14 @@ public class App
     public static void main( String[] args )
     {
     	
-        List<InterviewFileFeeder> fileds = readFromCSV("Inter.csv");
+        List<InterviewFileFeeder> fileds = readFromCSV("test.csv");
         // print all the fields read from CSV file
 
-    	
+    	/*
         for (InterviewFileFeeder field : fileds) {
             System.out.println(field);
         }
-        
+        */
     	
     	
         
@@ -39,12 +39,10 @@ public class App
             while (line != null) {
                 // use string.split to load a string array with the values from each line of the file, using regex as the delimiter
                 String[] attributes = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-                
-                //se poate de comentat tot inainte de doua stringri sau male sau female si dupa coloana f cu stringurile switch .....
-
+               
                 InterviewFileFeeder field = createField(attributes);
                 // adding field into ArrayList
-                System.out.println(field);
+                //System.out.println(field);
                 fields.add(field);
                 // read next line before looping
                 // if end of file reached, line would be null
@@ -60,29 +58,30 @@ public class App
 
     private static InterviewFileFeeder createField(String[] metadata) {
 
-    	String nameA = metadata[0];
-    	System.out.println(nameA);
-        String surnameB = metadata[1];
-        String mailC = metadata[2];
-        String genderD = metadata[3];
-        String profileImageE = metadata[4];
-        String paymentMethodF = metadata[5];
-        String transactionsG = metadata[6];
-        boolean h = Boolean.parseBoolean(metadata[7]);
-        /*
-        if(h == "") {
-        	
-        }*/
-        boolean i = Boolean.parseBoolean(metadata[8]);
-        String cityJ = metadata[9];
-    	System.out.println(cityJ);
-
-        System.out.println(metadata.length);
-//my out of index error profileImageE="data:image/png;base64, paymentMethodF=iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9
-
-        // create and return InterviewFileFeeder of this metadata
-        return new InterviewFileFeeder(nameA, surnameB, mailC, genderD, profileImageE, paymentMethodF, transactionsG, h, i, cityJ);
-         
+    	if(metadata.length < 10) {
+    		System.out.println("record has only " + metadata.length);
+    		//to do: Records that do not match the column count must be written to the bad-data-<timestamp>.csv file 
+    		
+    		return null;
+    	}else {
+        	String nameA = metadata[0];
+            String surnameB = metadata[1];
+            String mailC = metadata[2];
+            String genderD = metadata[3];
+            String profileImageE = metadata[4];
+            String paymentMethodF = metadata[5];
+            String transactionsG = metadata[6];
+            boolean h = Boolean.parseBoolean(metadata[7]);
+            boolean i = Boolean.parseBoolean(metadata[8]);        
+            String cityJ = metadata[9];
+        	//System.out.println("city " + cityJ);
+            //System.out.println(metadata.length);
+            // create and return InterviewFileFeeder of this metadata
+            return new InterviewFileFeeder(nameA, surnameB, mailC, genderD, profileImageE, paymentMethodF, transactionsG, h, i, cityJ);
+    	}
+    	
+    	//try to create the db and insert the fields
+    	
     }
 
 }
